3be32d2c1b90bd218698420c27c3fe680040ba33
Alexander Gromnitsky <alexander.gromnitsky@gmail.com>
2016-01-16 00:49:48 +0200

    client: parse filter opts
    react is too verbose;
    angular 1.x was much more useful

--- client/main.jsx
+++ client/main.jsx
@@ -2,16 +2,105 @@
 
 require("babel-polyfill")
 
+let util = require('util')
+
 let React = require('react')
 let ReactDOM = require('react-dom')
-let minimist = require('minimist')
+let shellquote = require('shell-quote')
 
-//let feed = require('../lib/feed')
+let u = require('../lib/u')
 
-//let grep = new feed.MyGrepXML({})
+let FeedBox = React.createClass({
+    getInitialState: function() {
+	return { url: '', filter: '' }
+    },
 
-ReactDOM.render(/*jshint ignore:start */
-	<span>Hello, World!</span>,
-    /*jshint ignore:end */ document.getElementById('result-opts'))
+    handle_feedForm: function(filter) {
+	this.setState({filter: filter})
+	console.log("FeedBox:handle_feedForm", filter)
+    },
 
-console.log("loaded")
+    handle_feedForm_submit: function(data) {
+	this.setState(data)
+	console.log("FeedBox:handle_feedForm_submit", data)
+    },
+
+    render: function() {
+	return (
+	    <div className="feedBox">
+	      <FeedForm on_data_change={this.handle_feedForm}
+			on_data_sumbit={this.handle_feedForm_submit} />
+	      <FeedArgv filter={this.state.filter} />
+	    </div>
+	)
+    }
+});
+
+let FeedForm = React.createClass({
+    getInitialState: function() {
+	return { url: '', filter: '' }
+    },
+
+    on_change_url: function(elm) {
+	this.setState({url: elm.target.value})
+    },
+
+    on_change_filter: function(elm) {
+	// call the parent
+	this.props.on_data_change(elm.target.value)
+	this.setState({filter: elm.target.value})
+    },
+
+    submit: function(elm) {
+	elm.preventDefault()
+	this.props.on_data_sumbit({url: this.state.url,
+				   filter: this.state.filter})
+    },
+
+    render: function() {
+	return (
+	    <form className="feedForm" onSubmit={this.submit}>
+	      <p><label>RSS URL:<br/>
+		  <input type="url"
+			 placeholder="http://example.com/rss.xml"
+			 value={this.state.url}
+			 onChange={this.on_change_url}
+			 required />
+		</label>
+	      </p>
+	      <p>
+		<label>Filter options: <code>
+		    [-d [-]date[,date]] [-c regexp]
+		    [-e] [-n digit] [-x] [-m] [regexp]
+		  </code><br />
+		  <input type="text" spellcheck="false"
+			 value={this.state.filter}
+			 onChange={this.on_change_filter}
+			 />
+		</label>
+	      </p>
+	      <p><input type="submit" />&nbsp;
+		<input type="reset" /></p>
+	    </form>
+	)
+    }
+});
+
+let FeedArgv = React.createClass({
+    filter2argv: function(str) {
+	return u.opts_parse(shellquote.parse(str))
+    },
+
+    render: function() {
+	return (
+	    <div className="feedArgv">
+	      <b>ARGV: </b>
+	      { util.inspect(this.filter2argv(this.props.filter), {depth: null }) }
+	    </div>
+	)
+    }
+});
+
+ReactDOM.render(
+    <FeedBox />,
+    document.getElementById('content'))
