9d5117f99bed3fa0b90061ca51258a19dd1cd95b
Alexander Gromnitsky <alexander.gromnitsky@gmail.com>
2018-03-23 21:40:38 +0200

    0.0.3

--- package.cpp
+++ package.cpp
@@ -7,7 +7,7 @@
 {
     "//": "DON'T EDIT! See package.cpp instead",
     "name": "grepfeed",
-    "version": "0.0.2",
+    "version": "0.0.3",
     "description": "Filters out rss/atom feeds. Returns articles matching a pattern. The output can be another valid xml feed.",
     "keywords": [
         "grep",
